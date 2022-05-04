1df2ea59fb50cfbd3fc12b0e37cf2ffe38bdb324
Анастасия Лузинсан <luzinsan@mail.ru>
2022-05-05 00:36:08 +0700

    добавлено поле размерности, метод find, поправлен вывод списка, геттеры и проверка на пустоту

--- lr7/ListNode.h
+++ lr7/ListNode.h
@@ -8,22 +8,23 @@ namespace luzinsan
 	class ListNode
 	{
 	private:
-		T* _info;              // информационное поле
-		ListNode<T>* _next;    // следующий элемент списка
-		ListNode<T>* _endList; // конец списка
+		T* _info;                // информационное поле
+		ListNode<T>* _next;      // следующий элемент списка
+		ListNode<T>* _endList;   // конец списка
+		size_t _size;
 	public:
-		ListNode* getNext() { return _next; }
-		ListNode() : _info{ NULL }, _next{ NULL } { }
-		ListNode(T info) : _info{new T}, _next { NULL }, _endList{ NULL }
+		ListNode() : _info{ NULL }, _next{ NULL }, _endList{ NULL } { }
+
+		ListNode(T info) : ListNode<T>()
 		{ 
+			_info = new T;
 			*_info = info;
+			_size++;
 		}
 
-		ListNode(const ListNode<T>& List) 
-			: ListNode<T>()
+		ListNode(const ListNode<T>& List) : ListNode<T>()
 		{
 			const ListNode<T>*  temp = &List;
-
 			while (temp != NULL)
 			{
 				append(*(temp->_info));
@@ -33,16 +34,19 @@ namespace luzinsan
 
 		~ListNode()
 		{
-			while (this->_next != NULL)
-				deleteNode(this);
+			while (this->_next != NULL) deleteNode(this);
 			delete _info;
 		}
+		bool isEmpty() { return !_info; }
 
+		ListNode* getNext() { return _next; }
+		size_t getSize() const { return _size; }
+		
 		// Вставка в конец списка
 		ListNode* append(T item)
 		{
 			ListNode<T>* currentItem = new ListNode<T>(item);
-			
+			_size++;
 			if (!_info)
 			{
 				*this = *currentItem;
@@ -51,7 +55,6 @@ namespace luzinsan
 			else
 			{
 				_endList->_next = currentItem; // добавляем после последнего элемента в списке новый элемент
-				//currentItem->_endList = _endList->_next;
 				_endList = _endList->_next;    // перемещаем указатель на новый последний элемент в списке
 			}
 			return this;
@@ -61,6 +64,7 @@ namespace luzinsan
 		// Вставка нового элемента списка после текущего, либо вставка первого элемента в начало
 		ListNode* insert(ListNode<T>* p, T i)
 		{
+			_size++;
 			ListNode<T>* q = new ListNode<T>;
 			assert(q && "Память не выделилась!!!");
 			q->l_info = i;
@@ -74,37 +78,69 @@ namespace luzinsan
 			return this;
 		}
 
+		// Поиск элемента списка по его значению. Возвращается указатель на предыдущий перед найденным элементом элемент.
+		// Если найдённый элемент первый по списку, то возвращается указатель на него самого.
+		ListNode<T>* find(const T& element)
+		{
+			ListNode<T>* prev_temp = this;
+			if (*(prev_temp->_info) == element)
+				return prev_temp;
+
+			const ListNode<T>* curr_temp = this;
+			for (int i = 1; i < _size; i++)
+			{
+				curr_temp = curr_temp->_next;
+				if(*(curr_temp->_info) == element)
+					return prev_temp;
+				prev_temp = prev_temp->_next;
+			}
+			return NULL;
+		}
+
+		T& operator[](int index) const
+		{
+			const ListNode<T>* temp = this;
+			for (int i = 0; i < index; i++)
+				temp = temp->_next;
+
+			return *(temp->_info);
+		}
+
 		//Удаление следующего элемента после текущего
 		T deleteNode(ListNode<T>* p)
 		{
-			T val = *(p->_next->_info);
-			delete p->_next->_info;
-			p->_next = p->_next->_next;
-			return val;
+			if (p && p->_next)
+			{
+				_size--;
+				T val = *(p->_next->_info);
+				delete p->_next->_info;
+				p->_next = p->_next->_next;
+				return val;
+			}
+			else return -1;
 		}
 
 		// Печать элементов списка
 		friend std::ostream& operator<<(std::ostream& out, const ListNode<T>& node)
 		{
 			const ListNode<T>* temp = &node;
-			std::cout << "Список:\n";
-			do
+			std::cout << "_|" << *(temp->_info) << "| -> ";
+			temp = temp->_next;
+			while (temp)
 			{
-				std::cout << *(temp->_info) << ' ';
+				std::cout << *(temp->_info) << " -> ";
 				temp = temp->_next;
-			} while (temp != NULL);
-			std::cout << "\n";
+			}
+			std::cout << "NULL\n_|_\n";
 			return out;
 		}
 
 		const ListNode<T>& operator=(T item) 
 		{
-			if (!_info)
-				_info = new T;
+			if (!_info) _info = new T;
 			*_info = item;
 			return *this;
 		}
-
 	};
 
 }
